b2a65ec5ce788db9e8ad2018a4b40cef63842b52
acohen999 <acohen999@gmail.com>
2015-01-26 21:26:55 +0000

    updatedW WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Aaron Cohen
+//Lab 01
+//Welcome Class Java Program
+//
+//first compile the program
+//    javac WelcomeClass.java
+//run the program
+//    java WelcomeClass//
+//
+//define a class
+public class WelcomeClass{
+    //add main method
+    public static void main(String[] args){
+        //display personal welcome from my Lehigh username with personal statement
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-A--R--C--3--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Two years ago I got to hold and feed a mother and baby sloth. It was a very cool experience for my girlfriend and I.");
+        //test/testtest
+    }
+}
